455a8486b4ad57484bc12ab7f369bf1701368aa9
PietervHa <vanhaaftenpieter@gmail.com>
2024-12-09 13:28:19 +0100

    Fxml verwijderd aangezien dit niet gebruikt mag worden, en voor nu even een flowplane ervoor terug toegevoegd

--- PigeonProject/src/main/java/com/pieter/pigeonproject/HelloApplication.java
+++ PigeonProject/src/main/java/com/pieter/pigeonproject/HelloApplication.java
@@ -3,6 +3,7 @@ package com.pieter.pigeonproject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +11,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FlowPane root = new FlowPane();
+        Scene scene = new Scene(root, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
